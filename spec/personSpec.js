d71ef3e87d0c583748977278093f1952889e4acc
Masixole <masixole.menqe@umuzi.org>
2019-09-25 15:20:02 +0200

    fixed folder structure

---
+++ spec/personSpec.js
@@ -0,0 +1,8 @@
+
+describe("the Person class as a new object", function() {
+    let max = new Person ('Max', 25, 'Male', ["playing soccer and coding"]);
+    it ("should be able to pass the instantiated arguments as values of the new object", function() {
+  
+       expect(max.hello('Max', 25, 'Male', ["playing soccer and coding"])).toBe('Hello, my name is Max and I am a 25 year old Male. My interests are playing soccer and coding.');
+    })
+})
