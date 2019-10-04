8743612206503aa58d172853a1a5bbd1258182b3
Masixole <masixole.menqe@umuzi.org>
2019-10-04 11:57:14 +0200

    revisited code

--- src/personSrc.js
+++ src/personSrc.js
@@ -12,7 +12,7 @@ class Person {
 }
 
 let max = new Person ('Max', 25, 'Male', ["playing soccer and coding"]);
-let masixole = new Person ('Yoliswa', 26, 'female', ["housekeeping, playing tennis and singing"]);
+let masixole = new Person ('Yoliswa', 30, 'female', ["housekeeping, playing tennis and singing"]);
 
 console.log(max.hello());
 console.log(masixole.hello());
