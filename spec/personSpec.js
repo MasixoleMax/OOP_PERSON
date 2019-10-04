2b9d3ebe1cace4d3e17410825a164792d95f7b21
Masixole <masixole.menqe@umuzi.org>
2019-10-04 15:45:48 +0200

    corrected a few mistakes

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -2,7 +2,7 @@
 describe("the Person class as a new object", function() {
 
     let  max = new Person('Max', 25, 'Male', ["playing soccer and coding"]);
-    let  masixole = new Person('Yoliswa', 26, 'female', ["housekeeping, playing tennis and singing"]);
+    let masixole = new Person ('Yoliswa', 30, 'female', ["playing tennis, housekeeping and singing"]);
 
     it ("should be able to pass the instantiated arguments as values of the new object", function() {
   
@@ -11,9 +11,6 @@ describe("the Person class as a new object", function() {
 
     it ("should be able to pass the instantiated arguments as values of the new object", function() {
   
-        expect(masixole.hello()).toBe('Hello, my name is Yoliswa and I am a 26 year old female. My interests are housekeeping, playing tennis and singing.');
-     });
+        expect(masixole.hello()).toBe('Hello, my name is Yoliswa and I am a 30 year old female. My interests are playing tennis, housekeeping and singing.');
+   });
 });
-       expect(max.hello('Max', 25, 'Male', ["playing soccer and coding"])).toBe('Hello, my name is Max and I am a 26 year old Male. My interests are playing soccer and coding.');
-    })
-})
